c7ab571b4b1abde793941b06147dc69867818fcc
ThePixelbrain <pixelbrain.lp@gmail.com>
2026-05-03 13:56:23 +0200

    Disable on other platforms, no standalone, bump version

--- src/main/java/io/dogboy/nilmacosfixes/NilMacosFixes.java
+++ src/main/java/io/dogboy/nilmacosfixes/NilMacosFixes.java
@@ -4,11 +4,18 @@ import nilloader.api.ClassTransformer;
 import nilloader.api.ModRemapper;
 import nilloader.api.NilLogger;
 
+import java.util.Locale;
+
 public class NilMacosFixes implements Runnable {
     public static final NilLogger log = NilLogger.get("NilMacosFixes");
 
     @Override
     public void run() {
+        if (!System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("mac")) {
+            log.info("Game was not launched on macOS, disabling NilMacosFixes");
+            return;
+        }
+
         log.info("Initializing NilMacosFixes...");
         ModRemapper.setTargetMapping("default");
 
